81512238a93c0d4453159b8385c1387ec28a370d
Tristan Slater <hello@trslater.ca>
2022-02-16 11:26:55 -0800

    Add truth table class

--- n_ary_logic.py
+++ n_ary_logic.py
@@ -1,6 +1,9 @@
 """An experiment with n-ary logic"""
 
+from contextlib import redirect_stdout
 from dataclasses import dataclass
+from io import StringIO
+from math import ceil, log
 
 
 @dataclass(order=True, slots=True, frozen=True)
@@ -38,3 +41,31 @@ class NAryDigit:
 
     def __format__(self, fmt: str) -> str:
         return f"{self.value:{fmt}}"
+
+
+class TruthTable:
+    def __init__(self, op, n=2) -> None:
+        self.op = op
+        self.n = n
+
+        self.truth_table = tuple(
+            tuple(op(NAryDigit(i, n=n), NAryDigit(j, n=n))
+                  for j in range(n))
+            for i in range(n))
+
+    def __str__(self) -> str:
+        w = ceil(log(self.n, 10))
+
+        # Capture prints to string
+        with redirect_stdout(StringIO()) as s:
+            print(" "*(w + 1) + "| ", end="")
+            print(" ".join(f"{i:{w}}" for i in range(self.n)))
+            print("-"*((1 + self.n)*(w + 1) + 1))
+            for i, row in enumerate(self.truth_table):
+                print(f"{i:>{w}} | ", end="")
+                for j, cell in enumerate(row):
+                    print(f"{cell:>{w}}", end=" ")
+                print()
+        s.seek(0)
+
+        return s.read()
