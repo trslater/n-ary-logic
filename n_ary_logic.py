a3324b81c8817d406642c7f48c839629d830356d
Tristan Slater <hello@trslater.ca>
2022-02-16 10:52:23 -0800

    Check compatibility of digits

--- n_ary_logic.py
+++ n_ary_logic.py
@@ -12,13 +12,22 @@ class NAryDigit:
         if not (0 <= self.value < self.n):
             raise ValueError(f"Value must be in [0, {self.n})")
 
+    def validate(self, other):
+        if self.n != other.n:
+            raise ValueError("Incompatible digits: "
+                             f"{repr(self)} and {repr(other)}")
+
     def __invert__(self):
         return self.__class__(self.n - self.value - 1, n=self.n)
 
     def __or__(self, other):
+        self.validate(other)
+
         return self.__class__(max(self.value, other.value), n=self.n)
 
     def __and__(self, other):
+        self.validate(other)
+
         return self.__class__(min(self.value, other.value), n=self.n)
 
     def __xor__(self, other):
